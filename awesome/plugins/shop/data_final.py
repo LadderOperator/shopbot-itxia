a771eed8b95ed5e841a37e53d1b102098ec89bae
LadderOperator <tz_ji@qq.com>
2019-07-25 17:46:10 +0800

    simplify the code using pandas & remove bs4
    pandas真好用
    bs4用这边有点啰嗦

--- awesome/plugins/shop/data_final.py
+++ awesome/plugins/shop/data_final.py
@@ -6,29 +6,22 @@ Created on Wed Jul 24 13:02:57 2019
 """
 
 import re
-from bs4 import BeautifulSoup
+import pandas as pd
 import requests as req
 
 async def get_the_final(result: str) -> str:
+#def get_the_final(result: str) -> str:
     #获得五个回答
     global msg
     msg = ''
     r_res = re.compile('(\d)\s+(\d)\s+(\d)\s+(\d)\s+(\d+)\W+(\d+)')
     m_res = re.match(r_res,result)
-    a1 = m_res.group(1)
-    a2 = m_res.group(2)
-    a3 = m_res.group(3)
-    a4 = m_res.group(4)
-    minm = m_res.group(5)
-    maxm = m_res.group(6)
+    a1,a2,a3,a4,minm,maxm = m_res.groups()
     
-    td = re.compile('<td.*?>(.*?)</td>')
     url = 'https://www.yuque.com/api/v2/repos/385391/docs/2185141'
     url_head = {'User-Agent':'Mozilla/5.0','content-type':'application/json','X-Auth-Token':'{token}'}
     r = req.get(url,headers = url_head)
-    r_content = BeautifulSoup(r.text,'html.parser')
-    r_content_table = r_content.find_all('tr')
-    #print(r_content_table)
+    r_content = r.text.replace('\\"','"')
     #创建分数统计列表
     goal=['']
     #创建+-统计
@@ -36,67 +29,57 @@ async def get_the_final(result: str) -> str:
     list2=['']
     #计数
     num = 1
-    for e_tr in r_content_table[1:]:
+    pd_table = pd.read_html(r_content, encoding='utf-8', header=0)[0]
+    PCindex = list(pd_table.index)
+
+    for e_tr in PCindex:
         #初始化三个表
         goal.append('')
         list1.append('')
         list2.append('')
-        
-        s_e_tr = str(e_tr)
-        r_td = re.findall(td,s_e_tr)
-        mingcheng = r_td[1].replace('\\n','').replace('<p>','').replace('</p>','').replace('<br/>','')
-        chuliqi = r_td[2].replace('\\n','').replace('<p>','').replace('</p>','').replace('<br/>','')
-        pseyu = r_td[3].replace('\\n','').replace('<p>','').replace('</p>','').replace('<br/>','')
-
-        neichunsj = r_td[6].replace('\\n','').replace('<p>','').replace('</p>','').replace('<br/>','')
-        neichun = r_td[7].replace('\\n','').replace('<p>','').replace('</p>','').replace('<br/>','')
-        seyu = r_td[8].replace('\\n','').replace('<p>','').replace('</p>','').replace('<br/>','')
-        shoujia = r_td[9].replace('\\n','').replace('<p>','').replace('</p>','').replace('<br/>','')
-        yongtu = r_td[11].replace('\\n','').replace('<p>','').replace('</p>','').replace('<br/>','')
-        pinmu = r_td[12].replace('\\n','').replace('<p>','').replace('</p>','').replace('<br/>','')
-        
-        if neichunsj != '0':
-            if a1 != '1':
+            
+        if  pd_table.loc[e_tr,'内存升级'] != 0:
+            if a1 != 1:
                 list1[num] = list1[num]+'+'
-            if a3 == '1':
+            if a3 == 1:
                 list1[num] = list1[num]+'+'
-        if neichun == '16':
-            if a1 == '1':
+        if pd_table.loc[e_tr,'内存/G'] == 16:
+            if a1 == 1:
                 list2[num] = list2[num]+'-'
-            if a1 == '3' or a1 == '4':
+            if a1 == 3 or a1 == 4:
                 list1[num] = list1[num]+'+'
-            if a3 != '1':
+            if a3 != 1:
                 list1[num] = list1[num]+'+'
-        if seyu == '0':
-            if a3 == '1':
+        if pd_table.loc[e_tr,'屏幕色域（数字为%）'] == 0:
+            if a3 == 1:
                 list2[num] += '-'
-        if yongtu == '3':
-            if a1 == '1':
+        if pd_table.loc[e_tr,'用途'] == 3:
+            if a1 == 1:
                 list2[num] += '-'
-            if a2 == '1':
+            if a2 == 1:
                 list2[num] += '-'
-            if a2 == '3':
+            if a2 == 3:
                 list2[num] += '+'
-        if pinmu == '1' or pinmu =='2':
-            if a1 == '3':
+        if pd_table.loc[e_tr,'屏幕描述'] == 1 or pd_table.loc[e_tr,'屏幕描述'] == 2:
+            if a1 == 3:
                 list1[num] += '+'
-            if a3 != '1':
+            if a3 != 1:
                 list1[num] += '+'
             
     
         if len(list1[num]) > 0:
             if len(list1[num]) > len(list2[num]):
-                if int(minm) < int(shoujia) and int(shoujia) < int(maxm):
-                    msg += '\n'+'本款推荐：'+mingcheng
-                    msg += '\n'+'处理器配置：'+chuliqi
-                    msg += '\n'+'色域：'+pseyu
-                    msg += '\n'+'售价：'+shoujia+'\n'
+                if int(minm) < int(pd_table.loc[e_tr,'参考售价/元']) and int(pd_table.loc[e_tr,'参考售价/元']) < int(maxm):
+                    msg += '\n'+'本款推荐：%s' % pd_table.loc[e_tr,'产品型号']
+                    msg += '\n'+'处理器配置：%s' % pd_table.loc[e_tr,'处理器']
+                    msg += '\n'+'色域：%s' % pd_table.loc[e_tr,'屏幕色域（数字为%）']
+                    msg += '\n'+'参考售价：%d' % pd_table.loc[e_tr,'参考售价/元'] +'\n'
                     goal[num] = list1[num]
             if len(list1[num]) == 1 and len(list2[num]) > 1:
-                msg += '\n'+'本款推荐：'+mingcheng
-                msg += '\n'+'处理器配置：'+chuliqi
-                msg += '\n'+'色域：'+pseyu
-                msg += '\n'+'售价：'+shoujia+'\n'
+                msg += '\n'+'本款推荐：%s' % pd_table.loc[e_tr,'产品型号']
+                msg += '\n'+'处理器配置：%s' % pd_table.loc[e_tr,'处理器']
+                msg += '\n'+'色域：%s' % pd_table.loc[e_tr,'屏幕色域（数字为%）']
+                msg += '\n'+'参考售价：%d' % pd_table.loc[e_tr,'参考售价/元']+'\n'
         else:
             goal[num]=''
         num += 1
@@ -104,3 +87,6 @@ async def get_the_final(result: str) -> str:
         return f'{msg}'
     except:
         return f'没有找到匹配的型号，尝试改变需求试试吧'
+    
+    
+#print(get_the_final("1 1 1 1 4000-5000"))
